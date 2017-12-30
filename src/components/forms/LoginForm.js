d9d55ab70cd5bdbec64c84bd9600b4370c60efb5
QAMR-ACCOUNTS\q835793 <sharish98@gmail.com>
2017-12-30 18:25:26 +0530

    added client side validations of log in form

---
+++ src/components/forms/LoginForm.js
@@ -0,0 +1,85 @@
+import React from "react";
+import {
+  Form,
+  FormGroup,
+  ControlLabel,
+  FormControl,
+  Button
+} from "react-bootstrap";
+import Validator from "validator";
+import PropTypes from "prop-types";
+import InlineError from "../messages/InlineError";
+
+class LoginForm extends React.Component {
+  state = {
+    data: {
+      email: "",
+      password: ""
+    },
+    loading: false,
+    errors: {}
+  };
+  onChange = e =>
+    this.setState({
+      data: { ...this.state.data, [e.target.name]: e.target.value }
+    });
+
+  onSubmit = event => {
+    event.preventDefault();
+    const errors = this.validate(this.state.data);
+    this.setState({ errors });
+    if (Object.keys(errors).length === 0) {
+      this.props.submit(this.state.data);
+    }
+  };
+
+  validate = data => {
+    const errors = {};
+    if (!Validator.isEmail(data.email)) errors.email = "Invalid email";
+    if (!data.password) errors.password = "can't be blank";
+    return errors;
+  };
+  render() {
+    const { data, errors } = this.state;
+
+    return (
+      <Form onSubmit={this.onSubmit}>
+        <FormGroup
+          controlId="email"
+          validationState={errors.email ? "error" : null}
+        >
+          <ControlLabel>Email</ControlLabel>
+          <FormControl
+            type="email"
+            name="email"
+            placeholder="example@example.com"
+            value={data.email}
+            onChange={this.onChange}
+          />
+          {errors.email && <InlineError text={errors.email} />}
+        </FormGroup>
+        <FormGroup
+          controlId="password"
+          validationState={errors.password ? "error" : null}
+        >
+          <ControlLabel>Password</ControlLabel>
+          <FormControl
+            type="password"
+            name="password"
+            placeholder="example@example.com"
+            value={data.password}
+            onChange={this.onChange}
+          />
+          {errors.password && <InlineError text={errors.password} />}
+        </FormGroup>
+        <Button bsStyle="primary" type="submit">
+          Login
+        </Button>
+      </Form>
+    );
+  }
+}
+LoginForm.propTypes = {
+  submit: PropTypes.func.isRequired
+};
+export default LoginForm;
