e048e9e9c7bf46c2f24a59a27e6b64983aecd428
QAMR-ACCOUNTS\q835793 <sharish98@gmail.com>
2017-12-30 11:15:06 +0530

    react bootstrap setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+
 
 ReactDOM.render(
   <BrowserRouter>
