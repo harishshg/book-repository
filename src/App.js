c56be8e6d25156a3611a29132c5682007712879b
QAMR-ACCOUNTS\q835793 <sharish98@gmail.com>
2017-12-31 23:20:31 +0530

    user sign up

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import PropTypes from "prop-types";
 import HomePage from "./components/pages/HomePage";
 import LoginPage from "./components/pages/LoginPage";
+import SignupPage from "./components/pages/SignupPage";
 import DashBoardPage from "./components/pages/DashBoardPage";
 import UserRoute from "./components/routes/UserRoute";
 import GuestRoute from "./components/routes/GuestRoute";
@@ -11,6 +12,12 @@ const App = ({ location }) => (
   <div className="container">
     <Route location={location} path="/" exact component={HomePage} />
     <GuestRoute location={location} path="/login" exact component={LoginPage} />
+    <GuestRoute
+      location={location}
+      path="/signup"
+      exact
+      component={SignupPage}
+    />
     <UserRoute
       location={location}
       path="/dashboard"
