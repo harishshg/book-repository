2e2c5b8ceaf8f3bfdfd56f091618ed835db99585
QAMR-ACCOUNTS\q835793 <sharish98@gmail.com>
2017-12-31 15:40:55 +0530

    login and logout

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,13 +1,28 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../../actions/auth";
 
-const HomePage = () => {
-  return (
-    <div>
-      <h1>Home Page</h1>
+const HomePage = ({ isAuthenticated, logout }) => (
+  <div>
+    <h1>Home Page</h1>
+    {isAuthenticated ? (
+      <button onClick={() => logout()}>Logout</button>
+    ) : (
       <Link to="/login">Login</Link>
-    </div>
-  );
+    )}
+  </div>
+);
+
+HomePage.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired,
+  logout: PropTypes.func.isRequired
 };
 
-export default HomePage;
+function mapStateToProps(state) {
+  return {
+    isAuthenticated: !!state.user.token
+  };
+}
+export default connect(mapStateToProps, { logout: actions.logout })(HomePage);
--- src/index.js
+++ src/index.js
@@ -10,11 +10,17 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import rootReducer from "./rootReducer";
+import { userLoggedIn } from "./actions/auth";
 
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
+
+if (localStorage.bookregistryJWT) {
+  const user = { token: localStorage.bookregistryJWT };
+  store.dispatch(userLoggedIn(user));
+}
 
 ReactDOM.render(
   <BrowserRouter>
